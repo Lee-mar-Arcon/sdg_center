c599b7ae655c4e834b4392accf39e0262316a583
Lee-mar <arconleemar0726.personal@gmail.com>
2024-10-12 20:44:06 +0800

    manages web routes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Page\WelcomeController;
 
 Route::get('/news', function () {
-   return Inertia::render('SDG_News');
+    return Inertia::render('SDG_News');
 });
 
 Route::get('/', function () {
@@ -20,8 +20,4 @@ Route::get('/mini', function () {
     return Inertia::render('SDG_mini');
 });
 
-<<<<<<< HEAD
-
-=======
-Route::get('/', [WelcomeController::class,'index'])->name('welcome');
->>>>>>> 344ad240f82c5a3776d3c14eee77825c1070c31f
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
