44a79c5dedf6f737b0a46802b375e02bd9aab32a
lee-mar <arconleemar0726.personal@gmail.com>
2024-11-09 16:59:32 +0800

    added article filter

--- resources/js/Pages/Admin/Article/Index.vue
+++ resources/js/Pages/Admin/Article/Index.vue
@@ -8,6 +8,18 @@
             </Link>
         </div>
         <div class="relative flex flex-col w-full h-full overflow-y-auto text-gray-700 bg-white shadow-md rounded-lg bg-clip-border">
+            <div class="relative max-w-[300px] pb-4">
+                <select
+                    v-model="SDGId"
+                    @change="e => updateArticleList(e)"
+                    class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded pl-3 pr-8 py-1.5 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer">
+                    <option value="All">All</option>
+                    <option v-for="sdg in props.sdgs" :key="sdg.id" :value="sdg.id">{{ sdg.name }}</option>
+                </select>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2" stroke="currentColor" class="h-5 w-5 ml-1 absolute top-2 right-2.5 text-slate-700">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                </svg>
+            </div>
             <table class="w-auto text-left table-auto">
                 <thead>
                     <tr class="border-b border-slate-300 bg-slate-50">
@@ -57,7 +69,7 @@
 
 <script setup>
 import AdminLayout from "@/Components/AdminLayout.vue";
-import { Link, useForm } from "@inertiajs/vue3";
+import { Link, useForm, router } from "@inertiajs/vue3";
 import {ref} from 'vue'
 const confirmationIsOpen = ref(false)
 function getLink(id){
@@ -65,6 +77,7 @@ function getLink(id){
 }
 const props = defineProps({
     articles: Array,
+    sdgs: Object
 })
 
 const deleteForm = useForm({
@@ -77,6 +90,15 @@ function handleDelete(id) {
             deleteForm.reset()
             confirmationIsOpen.value = false
         }
+    })
+}
+
+const SDGId = ref(route().params.sdg ? route().params.sdg : 'All')
+function updateArticleList(e) {
+    router.visit(route(route().current(), {sdg: e.target.value}), {
+        only: ['articles'],
+        preserveState: true,
+        preserveScroll: true,
     })
 }
 </script>
